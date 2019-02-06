5040724dc45c61b34c10d4c805f669ed9994f2db
Ewing Eng <eeng275@gmail.com>
2019-02-05 19:59:31 -0500

    finished registration

--- cheesebot.py
+++ cheesebot.py
@@ -2,25 +2,35 @@ import discord
 from discord.ext import commands
 
 description = '''This bot has several command functions.'''
-bot = commands.Bot(command_prefix='$', description=description)
+client = commands.Bot(command_prefix='$', description=description)
 
-@bot.event
+TOKEN = 'NTQyMTk4MTg4OTMzNjQ0Mjk4.DzrDPw.Ajg5xYwTGg5dvI7rs7Um0JFoEQE'
+
+
+@client.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
+    print(client.user.name)
+    print(client.user.id)
     print('\n')
 
 
-# Create an SQL database.
-@bot.command()
-async def signup(username):
-    if username:
-        msg = 'You signed up already {0.author.mention}!'.format()
-        await bot.send_message(username.channel, msg)
-        return
-    else:
-        # Add to database
-        return
+# Create a text file version
+@client.command(pass_context=True,
+                name='register',
+                description="Registers a player into the MMR system.")
+async def register(ctx):
+    file = open("data.txt", "w+")
+    file_s = file.read()
 
-bot.run('token')
+    if str(ctx.message.author.id) in file_s:
+        await client.say('You registered already ' + ctx.message.author.mention + "!")
+    else:
+        # Add to text file
+        file.write(str(ctx.message.author.id)+","+"0")
+        await client.say('Registration complete!')
+    file.close()
+    return
+
+
+client.run(TOKEN)
