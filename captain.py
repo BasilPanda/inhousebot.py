6560b4ff53949c8682f086e870e7ac281579eb60
Ewing Eng <35047506+BasilPanda@users.noreply.github.com>
2019-04-24 18:23:55 -0400

    Version 1.0 (#7)
    * started lobby
    
    * Update inhousebot.py
    
    * finished queue system
    
    * updated version 1.0

---
+++ captain.py
@@ -0,0 +1,61 @@
+from lobby import *
+
+
+class Captain:
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Allows only captains to report match results
+    @commands.command(pass_context=True,
+                      name='report',
+                      help="Report match results. Must be a captain of a team.",
+                      aliases=['r'])
+    async def report(self, ctx):
+        # Checks to see if any lobbies are running first
+        if not lobby1 and not lobby2 and not lobby3 and not lobby4:
+            await self.bot.say(ctx.message.author.mention + ", there are no lobbies running!")
+            return
+
+        lobby_num = is_captain(ctx.message.author.id)
+        # Checks to see if the user reporting is a team captain
+        if lobby_num == 0:
+            await self.bot.say(ctx.message.author.mention + ", only captains can report!")
+            return
+        try:
+            command, result = ctx.message.content.split(" ", 1)
+        except ValueError:
+            await self.bot.say(ctx.message.author.mention + ", please provide a match result!\nEx) "
+                                                            "$report win or %report loss")
+            return
+        # Handling erroneous parameters
+        if result.lower() not in lose_arr and result.lower() not in win_arr:
+            await self.bot.say(ctx.message.author.mention + ", please provide a correctly formatted match result!\nEx) "
+                                                            "$report win or %report loss")
+        if lobby_num == 1:
+            if lobby1:
+                # Create match ID 
+                match_id = db.create_match(db_connection)
+                if ctx.message.author.id == str(lob1_b[0].id) and result.lower() in lose_arr:
+                    win_embed, lose_embed = adjust_teams(lob1_r, lob1_b, match_id)
+                elif ctx.message.author.id == str(lob1_b[0].id) and result.lower() in win_arr:
+                    win_embed, lose_embed = adjust_teams(lob1_b, lob1_r, match_id)
+                elif ctx.message.author.id == str(lob1_r[0].id) and result.lower() in lose_arr:
+                    win_embed, lose_embed = adjust_teams(lob1_b, lob1_r, match_id)
+                elif ctx.message.author.id == str(lob1_r[0].id) and result.lower() in win_arr:
+                    win_embed, lose_embed = adjust_teams(lob1_r, lob1_b, match_id)                
+                for x in range(len(lob1_b)):
+                    db.update_player(db_connection, lob1_b[x])
+                    db.update_player(db_connection, lob1_r[x])
+                lob1_b.clear()
+                lob1_r.clear()
+                await self.bot.say(embed=win_embed)
+                await self.bot.say(embed=lose_embed)
+                lobby1.clear()
+            else:
+                await self.bot.say(ctx.message.author.mention + ", that lobby isn't active!")
+
+    # ADD OTHER LOBBIES
+
+
+def setup(bot):
+    bot.add_cog(Captain(bot))
--- inhousebot.py
+++ inhousebot.py
@@ -1,21 +1,50 @@
 import discord
+import datetime
+import math
+import requests
+import json
+import config
+import sys
 from discord.ext import commands
 from Player import Player
-
-import csv
+from database import Database as db
 import datetime
-import config
 
-description = '''This bot has several command functions.'''
+description = '''Inhouse Bot Help Manual:'''
 client = commands.Bot(command_prefix='$', description=description)
-
+client.remove_command('help')
+db_connection = db.init_database()
 TOKEN = config.bot_token
+API_KEY = config.bot_token
 
+startup_extensions = ["test_cmds", "user", "admin", "captain"]
+
+#   CONSTANTS
+INIT_WINS = 0
+INIT_LOSS = 0
+INIT_STREAK = 0
+INIT_ELO = 1500
+
+# four global lobbies
 lobby1 = []
 lobby2 = []
 lobby3 = []
 lobby4 = []
-in_queue = 0
+# teams for each lobby
+lob1_b = []
+lob1_r = []
+lob2_b = []
+lob2_r = []
+lob3_b = []
+lob3_r = []
+lob4_b = []
+lob4_r = []
+# waiting queue
+in_queue = []
+test_queue = []
+# lists
+lose_arr = ["lose", "loss", "l"]
+win_arr = ["win", "won", "w"]
 
 
 @client.event
@@ -25,66 +54,77 @@ async def on_ready():
     print(client.user.id)
     print('\n')
 
-
-# Create a text file version
-@client.command(pass_context=True,
-                name='register',
-                description="Registers a player into the inhouse system.")
-async def register(ctx, *args):
-    if not check_database(ctx):
-        with open('database.csv', 'a', newline='') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            ign = " ".join(args)
-
-            # Row setup as DATE,ID,ELO,IGN,WINS,LOSSES,STREAK
-            currentTime = datetime.datetime.now()
-            strTime = str(currentTime.month) + "-" + str(currentTime.day) + "-" + str(currentTime.year)
-            filewriter.writerow([strTime, ctx.message.author.id, str(500), ign, '0', '0', '0'])
-        await client.say('Registration complete ' + ctx.message.author.mention + "!")
-        return
-    else:
-        await client.say('You registered already ' + ctx.message.author.mention + "!")
-    return
+    # To check current API
+    r = requests.get(
+        "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Riot Phreak?api_key="
+        + config.lol_token)
+    response_json = r.json()
+    # print(response_json)
+    if 'status' in response_json:
+        if 'Forbidden' in response_json['status']['message']:
+            print("Please update League API key!")
+            sys.exit(-1)
 
 
-# Enter the queue
-@client.command(pass_context=True,
-                name='queue',
-                aliases=['join'])
-async def queue(ctx):
-    if check_database(ctx):
-        await client.say('You are not registered yet! Use $register ign to become join inhouses!')
-    else:
-        p = Player()
-        if len(lobby1) < 10:
-            lobby1.append()
-    return
+# Checks to see if user sending command is a captain
+def is_captain(user_id):
+    # assuming lobbies are already sorted
+    if len(lobby1) > 0:
+        for player in lobby1:
+            # if the ID is a match and that match is either the 1st or 2nd player in an ELO-sorted lobby
+            if str(player.id) == user_id and (lobby1.index(player) == 0 or lobby1.index(player) == 1):
+                return 1
+    elif len(lobby2) > 0:
+        for player in lobby2:
+            if str(player.id) == user_id and (lobby2.index(player) == 0 or lobby2.index(player) == 1):
+                return 2
+    elif len(lobby3) > 0:
+        for player in lobby3:
+            if str(player.id) == user_id and (lobby3.index(player) == 0 or lobby3.index(player) == 1):
+                return 3
+    elif len(lobby4) > 0:
+        for player in lobby4:
+            if str(player.id) == user_id and (lobby4.index(player) == 0 or lobby4.index(player) == 1):
+                return 4
+    return 0
 
 
-def check_database(ctx):
-    with open('database.csv', 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-        for row in reader:
-            if ctx.message.author.id == row[1]:
-                return True
-        return False
+# Create provider ID
+def create_provider_id(region, callback_url):
+    url = 'https://americas.api.riotgames.com/lol/tournament-stub/v4/providers?api_key=' + config.lol_token
+    data = {"region": region, "url": callback_url}
+    postRequest = requests.post(url, data=json.dumps(data), verify=True)
+    postJson = postRequest.json()
+    return postJson
 
 
-def get_player(ctx):
-    p = Player()
-    with open('database.csv', 'r') as f:
-        reader = csv.reader(f, delimiter=',')
-        for row in reader:
-            if ctx.message.author.id == row[1]:
-                p.date = row[0]
-                p.id = row[1]
-                p.elo = row[2]
-                p.ign = row[3]
-                p.wins = row[4]
-                p.losses = row[5]
-                p.streak = row[6]
-                break
-    return p
+# Create tournament ID
+def create_tournament_id(name, provider_id):
+    url = 'https://americas.api.riotgames.com/lol/tournament-stub/v4/tournaments?api_key=' + config.lol_token
+    data = {"name": name, "providerId": provider_id}
+    postRequest = requests.post(url, data=json.dumps(data), verify=True)
+    postJson = postRequest.json()
+    tourn = create_tournament_game(postJson)
+    return tourn
 
 
-client.run(TOKEN)
+def create_tournament_game(tournament_id):
+    count = 10
+    url = 'https://americas.api.riotgames.com/lol/tournament-stub/v4/codes?count=' + count + '&tournamentId=' + tournament_id + '&api_key=' + config.lol_token
+    data = {"mapType": "SUMMONERS_RIFT", "pickType": "DRAFT_MODE", "spectatorType": "ALL", "teamSize": 5}
+    headers = {'content-type': 'application/json'}
+    postRequest = requests.post(url, data=json.dumps(data), verify=True)
+    # contains list of the tournament IDs
+    postJson = postRequest.json()
+    return postJson
+
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            client.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+    client.run(TOKEN)
+
